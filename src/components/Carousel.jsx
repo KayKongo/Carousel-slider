7afa252427c3a8002b8867521527880d16316806
kaykongo <pkkongo@st.ug.edu.gh>
2025-05-24 19:32:55 +0000

    feat: added the previous and next buttons to the UI

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,8 +1,18 @@
 import { useState } from "react";
 import { shortList, longList, list } from "../data";
+import { FaQuoteRight } from "react-icons/fa";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 
 const Carousel = () => {
   const [people, setPeople] = useState(shortList);
+
+  const prevSlide = () => {
+    console.log("you clicked the previous button!");
+  };
+  const nextSlide = () => {
+    console.log("you clicked the next button!");
+  };
+
   return (
     <section className="slider-container">
       {people.map((person) => {
@@ -13,9 +23,18 @@ const Carousel = () => {
             <h5 className="name">{name}</h5>
             <p className="title">{title}</p>
             <p className="text">{quote}</p>
+            <FaQuoteRight className="icon" />
           </article>
         );
       })}
+
+      {/* Buttons */}
+      <button type="button" className="prev" onClick={() => prevSlide()}>
+        <FiChevronLeft />
+      </button>
+      <button type="button" className="next" onClick={() => nextSlide()}>
+        <FiChevronRight />
+      </button>
     </section>
   );
 };
